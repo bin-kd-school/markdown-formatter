211623697a7396df89f1aebfc1faf56a59671f68
makanai-j <kd1365575@st.kobedenshi.ac.jp>
2024-06-27 14:58:39 +0900

    AddSpaceSymbolsにコメント追加

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+/**
+ * マークダウンを正しい記法に直し読みやすいものへと修正する
+ */
 class MdFmtr {
   // 6/27  追加
-  // md記法で使われる記号の後にスペースを一つ挿入
+  /**
+   * md記法で使われる記号の後にスペースを一つ挿入
+   * @type {(orgText: string[]) => string[]}
+   */
   AddSpaceSymbols(orgText) {
-    // 正規表現パターンを配列に定義
+    /**
+     * 第1引数: 正規表現パターン
+     * 第2引数: 置換用の文字列
+     * @type {{regex: pattern, replacement: string}}
+     */
     const patterns = [
       { regex: /^([-*+]{2})( *)(\S)/, replacement: "$1$3" }, // 2個以上の場合は協調になるのでskip
       // 箇条書きリストと番号付きリストは
