d081b1a89e5c5de5a8ef6a53bf9f5d03c23f1e6d
makanai-j <kd1365575@st.kobedenshi.ac.jp>
2024-07-01 09:25:29 +0900

    indentをそろえるメソッド修正

--- test/mdfmtrTest.js
+++ test/mdfmtrTest.js
@@ -3,9 +3,7 @@ import { ProofreadUtil } from "../src/ProofreadUtil.js";
 import { argArray, argStr } from "./argument.js";
 
 console.log(argArray);
-/*
+
 const mdfmtr = new MdFmtr(argStr);
 console.log(mdfmtr.fixedMdText);
 console.log(mdfmtr.errors);
-*/
-console.log(ProofreadUtil.all(argArray));
